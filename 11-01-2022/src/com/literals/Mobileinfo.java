f026a7215f25c9a65ad88d89bdab530746ff4ace
Umagreeg-40 <umamahesankaranss@gmail.com>
2022-01-26 17:06:05 -0800

    Fourth project

---
+++ 11-01-2022/src/com/literals/Mobileinfo.java
@@ -0,0 +1,26 @@
+package com.literals;
+
+public class Mobileinfo {
+
+	public static void main(String[] args) {
+		
+		String brand= "OppoReno6";
+		String model= "11i 5g";
+		int price= 30000;
+		String colour= "Gray";
+		String ram= "8 gb";
+		String rom= "0s11version";
+		long imei= 860409050;
+		
+		System.out.println("Brand: "+brand);
+		System.out.println("Model: "+model);
+		System.out.println("Colour: "+colour);
+		System.out.println("ROM: "+ram);
+		System.out.println("ROM: "+rom);
+		System.out.println("Price: "+price);
+		System.out.println("IMEI number: "+imei);
+		
+		
+	}
+
+}
